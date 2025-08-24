464cc83050ad25ad1944a1f9292c9937a959438c
Xeltry <xaero1616@gmail.com>
2025-08-24 21:18:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
 # === Настройки через переменные окружения ===
-TOKEN = os.environ["8377153990:AAFy4pG_UH109pgfxe_TGAiXTB6waKyU7YE"]         # токен бота от BotFather
-BASE_URL = os.environ["BASE_URL=https://telegram-queue-bot-98zw.onrender.com"]                # домен Render, например https://mybot.onrender.com
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]  # имя переменной окружения
+BASE_URL = os.environ["BASE_URL"]         # имя переменной окружения
 PORT = int(os.environ.get("PORT", 10000))        # Render сам подставляет PORT
 
 DATA_FILE = "queues.json"
