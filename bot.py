7cc5a81df9c3d8b8043d6075f47b583e85edc464
Xeltry <xaero1616@gmail.com>
2025-10-02 21:43:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -252,4 +252,5 @@ async def health():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("bot:app", host="0.0.0.0", 
+    uvicorn.run("bot:app", host="0.0.0.0", port=port)
+    
