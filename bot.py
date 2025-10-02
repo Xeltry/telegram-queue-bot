1ba7a8267b0a21d567c89d6eccee315d3158dd9a
Xeltry <xaero1616@gmail.com>
2025-10-02 14:08:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 # ====== Окружение ======
 TOKEN    = os.getenv("8377153990:AAFy4pG_UH109pgfxe_TGAiXTB6waKyU7YE")
-BASE_URL = os.getenv("https://telegram-queue-bot-u1re.onrender.com")
+BASE_URL = os.getenv("https://telegram-queue-bot.onrender.com")
 if not TOKEN or not BASE_URL:
     raise RuntimeError("Не заданы TELEGRAM_BOT_TOKEN или BASE_URL")
 
@@ -221,4 +221,5 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("bot:app", host="0.0.0.0", port=port)
+
 
